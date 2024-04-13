be0909fbbb87d40d6a1aa640344fb1439c871376
Ken Ellorando <kenellorando@gmail.com>
2024-04-13 16:25:30 -0500

    Turn off cluster

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # infra
-My personal cloud infrastructure as code.
+My ([kenellorando's](https://github.com/kenellorando)) personal cloud infrastructure as code.
 
 ## Directory
 
@@ -8,9 +8,10 @@ main/               -   Terraform to build cluster and pet servers.
 cluster-service/    -   Scripts to install cluster add-ons.
 ```
 
-
 ## Install
 1. Install Gcloud, Terraform, and Kubectl.
 2. `gcloud auth application-default login`
-3. In `cluster/`, run `tf init` then `tf apply` to build the cluster.
-4. Run `gcloud container clusters get-credentials CLUSTER_NAME --region=CLUSTER_REGION` to create a `kubeconfig` for your `kubectl`.
+3. In `main/`, run `tf init` then `tf apply` to build.
+
+### Connect to Cluster 
+1. Run `gcloud container clusters get-credentials CLUSTER_NAME --region=CLUSTER_REGION` to generate a `kubeconfig` for `kubectl`.
--- main/cluster.tf
+++ main/cluster.tf
@@ -1,48 +1,48 @@
-resource "google_container_cluster" "alpha" {
-  name     = "alpha"
-  location = "us-central1-a"
+# resource "google_container_cluster" "alpha" {
+#   name     = "alpha"
+#   location = "us-central1-a"
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
-  initial_node_count       = 1
+#   # We can't create a cluster with no node pool defined, but we want to only use
+#   # separately managed node pools. So we create the smallest possible default
+#   # node pool and immediately delete it.
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
 
-  addons_config {
-    http_load_balancing {
-      disabled = true
-    }
+#   addons_config {
+#     http_load_balancing {
+#       disabled = true
+#     }
 
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
-}
+#     horizontal_pod_autoscaling {
+#       disabled = false
+#     }
+#   }
+# }
 
-resource "google_container_node_pool" "alpha_pool_general" {
-  name       = "general"
-  location   = "us-central1-a"
-  cluster    = google_container_cluster.alpha.name
-  node_count = 0
+# resource "google_container_node_pool" "alpha_pool_general" {
+#   name       = "general"
+#   location   = "us-central1-a"
+#   cluster    = google_container_cluster.alpha.name
+#   node_count = 0
 
-  node_config {
-    spot = true
-    machine_type = "e2-small"
+#   node_config {
+#     spot = true
+#     machine_type = "e2-small"
 
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     service_account = google_service_account.default.email
+#     oauth_scopes    = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
 
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.alpha.name
-  description = "GKE Cluster Name"
-}
+# output "kubernetes_cluster_name" {
+#   value       = google_container_cluster.alpha.name
+#   description = "GKE Cluster Name"
+# }
 
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.alpha.endpoint
-  description = "GKE Cluster Host"
-}
+# output "kubernetes_cluster_host" {
+#   value       = google_container_cluster.alpha.endpoint
+#   description = "GKE Cluster Host"
+# }
